6d550a0c86cbfd79a82f9e8d70095ecbf7b4fb07
Amrit Pritam Mohanty <amritpritam226@gmail.com>
2025-02-25 12:46:06 +0530

    Update Challege_Accepted.java

--- Challege_Accepted.java
+++ Challege_Accepted.java
@@ -28,27 +28,18 @@ public class Challege_Accepted {
 		
 		Random r=new Random();
 		int r1=r.nextInt(100);
-		
-		
 		Guess su[]=new Guess[8];
-		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("You have only Eight guesses");
-		
 		for(int i=1;i<=8;i++)
 		{	
 				int a=8-i;
 				System.out.println();
 				System.out.println("Enter the number For "+i+ " "+"guess and your"+" "+a+" "+" guess remaining");
 				int num=sc.nextInt();
-				su[i]= new Guess(num,r1,i);
-			
-			
+				su[i]= new Guess(num,r1,i);			
 		}
 		System.out.println("Opps!! you lose this game");
-		System.out.println("The Computer Number is:"+r1);
-		
-		
+		System.out.println("The Computer Number is:"+r1);	
 	}
-
 }
